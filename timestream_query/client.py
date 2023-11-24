da0deed5d09ee35b9d37e1d12f6c782f012114a7
matheus politano <7602799-matheuspolitano@users.noreply.gitlab.com>
2023-11-24 13:31:51 -0300

    add region

--- timestream_query/client.py
+++ timestream_query/client.py
@@ -5,13 +5,13 @@ from timestream_query.data_handle import QueryResult
 
 
 class TimeStreamQuery:
-    def __init__(self, session: Session = None):
+    def __init__(self, session: Session = None, region="us-east-1"):
         self.session = session
-        self.client = self._define_client()
+        self.client = self._define_client(region)
 
-    def _define_client(self):
+    def _define_client(self, region):
         if self.session is None:
-            return boto3.client("timestream-query")
+            return boto3.client("timestream-query", region_name=region)
         return self.session.client("timestream-query")
 
     def execute_query(self, query: str) -> QueryResult:
