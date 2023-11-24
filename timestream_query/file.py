066874d63353f3258d6aaa3f7146ea89869a3031
matheus politano <7602799-matheuspolitano@users.noreply.gitlab.com>
2023-11-24 14:31:49 -0300

    add txt reader

--- timestream_query/file.py
+++ timestream_query/file.py
@@ -13,9 +13,30 @@ def generate_csv(columns, data, path, delimiter=","):
     with open(path, "w", newline="", encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile, delimiter=delimiter)
 
-        # Write the header
         writer.writerow(columns)
 
-        # Write the data rows
         for row in data:
             writer.writerow(row)
+
+
+def read_text_file(file_path: str) -> str:
+    """
+    Reads the contents of a text file and returns it as a string.
+
+    Args:
+        file_path (str): The path to the text file.
+
+    Returns:
+        str: The contents of the file as a string.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        IOError: If an error occurs during file reading.
+    """
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            return file.read()
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File not found: {file_path}")
+    except IOError as e:
+        raise IOError(f"Error reading file {file_path}: {e}")
